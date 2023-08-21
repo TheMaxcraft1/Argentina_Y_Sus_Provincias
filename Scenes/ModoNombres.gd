a36e5c38f3a817177187b4c8ce623b08e759c985
Máximo Fain <maximofain0@gmail.com>
2023-08-21 19:18:53 -0300

    All LineEdits for Provinces

--- Scenes/ModoNombres.gd
+++ Scenes/ModoNombres.gd
@@ -1,32 +1,42 @@
 extends Node2D
 
 var score = 0
+var multiplier = 1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	score = 0
+	multiplier = 1
+	$HUD/MultiplierLabel.add_theme_font_size_override("font_size", 50)
+	$HUD/MultiplierLabel.add_theme_color_override("font_color", Color(255,255,255))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if multiplier > 3:
+		multiplier = 3
+	$HUD/FPSLabel.set_text(str(Engine.get_frames_per_second()))
 
 
-func _on_buenos_aires_line_edit_text_submitted(txt):
-	var was_submitted = false
-		
+func _on_buenos_aires_line_edit_text_submitted(txt):		
 	if txt.to_lower() == "buenos aires":
-		if was_submitted == false:
-			updateScoreLabel(100)
-			was_submitted = true
+		updateScoreLabel(100 * multiplier)
+		multiplier += 1
+	else:
+		multiplier = 1
+	updateMultiplierLabeel()
 	$BuenosAiresLineEdit.set_editable(false)
 	$BuenosAiresLineEdit.set_focus_mode(Control.FOCUS_NONE)
 	
 
 func _on_entre_rios_line_edit_text_submitted(txt):
 	if txt.to_lower() == "entre ríos":
-		updateScoreLabel(100)
+		updateScoreLabel(100 * multiplier)
+		multiplier += 1
+	else:
+		multiplier = 1
+	updateMultiplierLabeel()
 	$EntreRiosLineEdit.set_editable(false)
 	$EntreRiosLineEdit.set_focus_mode(Control.FOCUS_NONE)
 	
@@ -34,5 +44,20 @@ func updateScoreLabel(points):
 	score += points
 	$HUD/ScoreLabel.set_text(str(score))
 
+func updateMultiplierLabeel():
+	$HUD/MultiplierLabel.set_text(str(multiplier) + "X!")
+	if multiplier == 1:
+		$HUD/MultiplierLabel.add_theme_font_size_override("font_size", 50)
+		$HUD/MultiplierLabel.add_theme_color_override("font_color", Color(255,255,255))
+		$HUD/MultiplierLabel.set_rotation(0)
+	elif multiplier == 2:
+		$HUD/MultiplierLabel.add_theme_font_size_override("font_size", 60)
+		$HUD/MultiplierLabel.add_theme_color_override("font_color", Color(251,255,0))
+		$HUD/MultiplierLabel.set_rotation(deg_to_rad(15))
+	elif multiplier == 3:
+		$HUD/MultiplierLabel.add_theme_font_size_override("font_size", 75)
+		$HUD/MultiplierLabel.add_theme_color_override("font_color", Color(255,0,0))
+		$HUD/MultiplierLabel.set_rotation(deg_to_rad(30))
 
+	
 
